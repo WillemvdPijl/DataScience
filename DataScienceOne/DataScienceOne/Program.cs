40268c21c3fc3970318b2075641d73e862a59491
Willem_Pijl <willemvanderpijl@hotmail.com>
2018-03-08 12:58:12 +0100

    Opdracht 1.1 af

--- DataScienceOne/DataScienceOne/Program.cs
+++ DataScienceOne/DataScienceOne/Program.cs
@@ -11,15 +11,32 @@ namespace DataScienceOne
         static void Main(string[] args)
         {
             string[] userItem = System.IO.File.ReadAllLines(@"../../userItem.data");
-            Console.WriteLine(userItem[0]);
+
+            Dictionary<int, Dictionary<int, float>> result = new Dictionary<int, Dictionary<int, float>>();
 
             foreach (string item in userItem)
             {
 
+                Dictionary<int, float> rating = new Dictionary<int, float>();
+    
                 var user = item.Split(',');
 
-                Console.WriteLine(user);
+                rating.Add(int.Parse(user[1]), float.Parse(user[2]));
+
+                if (result.ContainsKey(int.Parse(user[0])))
+                {
+                    result[int.Parse(user[0])].Add(int.Parse(user[1]), float.Parse(user[2]));
+                }
+                else
+                {
+                    result.Add(int.Parse(user[0]), rating);
+                }
+                    
+
+                Console.WriteLine(result);
+
             }
+            Console.ReadKey();
         }
     }
 }
