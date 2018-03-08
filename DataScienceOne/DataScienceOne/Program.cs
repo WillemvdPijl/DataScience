02ebabc5f81d97002f0f13250ad35d6efdb89f00
Willem_Pijl <willemvanderpijl@hotmail.com>
2018-03-08 12:34:44 +0100

    Visual Studio Enterprise

---
+++ DataScienceOne/DataScienceOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScienceOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userItem = System.IO.File.ReadAllLines(@"../../userItem.data");
+            Console.WriteLine(userItem[0]);
+
+            foreach (string item in userItem)
+            {
+
+                var user = item.Split(',');
+
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
