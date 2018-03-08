8aab781824f8e96eca58c54701ab7bfe45e39eee
Willem_Pijl <willemvanderpijl@hotmail.com>
2018-03-08 14:59:48 +0100

    Console logs added

--- DataScienceOne/DataScienceOne/Program.cs
+++ DataScienceOne/DataScienceOne/Program.cs
@@ -10,9 +10,14 @@ namespace DataScienceOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Reading file...");
             string[] userItem = System.IO.File.ReadAllLines(@"../../userItem.data");
 
             Dictionary<int, Dictionary<int, float>> result = new Dictionary<int, Dictionary<int, float>>();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("========= Task 1 =========");
+            Console.WriteLine(" ");
 
             foreach (string item in userItem)
             {
@@ -26,16 +31,19 @@ namespace DataScienceOne
                 if (result.ContainsKey(int.Parse(user[0])))
                 {
                     result[int.Parse(user[0])].Add(int.Parse(user[1]), float.Parse(user[2]));
+                    Console.WriteLine("Adding to existing key...");
                 }
                 else
                 {
                     result.Add(int.Parse(user[0]), rating);
+                    Console.WriteLine("Adding to new key...");
                 }
-                    
-
-                Console.WriteLine(result);
-
             }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("========= Done =========");
+            Console.WriteLine(" ");
+            Console.WriteLine("Hit any key to exit...");
             Console.ReadKey();
         }
     }
